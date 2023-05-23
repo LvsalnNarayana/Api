ebf84c6bbad2ac423cc8b52153c0e2595434e4f3
LvsalnNarayana <lvsalnnarayana@gmail.com>
2023-05-23 17:54:45 +0100

    express session setup

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 import express, { urlencoded } from 'express';
-import { createServer } from 'http';
 import cors from 'cors';
+import { createServer } from 'http';
+import { sessionMiddleware } from './utils/SessionMiddleware';
 
 var app = express();
 var server = createServer({ rejectUnauthorized: false }, app);
+
 app.use(cors({
     origin: '*',
     credentials: true,
 }));
+
+app.use(sessionMiddleware);
+
 app.use(urlencoded({ extended: true }));
+
 app.get('/', (req, res, next) => {
+    req.session.user = null;
+    req.session.save();
     res.status(200).json({ "hello": "hello" });
 });
 
---
+++ utils/SessionMiddleware.js
@@ -0,0 +1,22 @@
+import session from 'express-session';
+import { default as connectMongodbSession } from 'connect-mongodb-session';
+import dotenv from 'dotenv';
+dotenv.config();
+const MongoDBStore = connectMongodbSession(session);
+export const store = new MongoDBStore({
+    uri: process.env.MONGODB_URI,
+    collection: 'sessions',
+});
+export const sessionMiddleware = session({
+    cookie: {
+        secure: false,
+        maxAge: 24 * 60 * 60 * 1000,
+        httpOnly: false,
+        sameSite: 'none'
+    },
+    name: 'test_it',
+    secret: process.env.JWT_SECRET,
+    saveUninitialized: false,
+    resave: true,
+    store: store,
+});
