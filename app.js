bfae52953b60d693a39a532a7d06462374d6f576
LvsalnNarayana <lvsalnnarayana@gmail.com>
2023-05-23 14:13:06 +0100

    just test ready to deploy

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
+import { createServer } from 'http';
+import cors from 'cors';
 
 var app = express();
+var server = createServer({ rejectUnauthorized: false }, app);
+app.use(cors({
+    origin: '*',
+    credentials: true,
+}));
+app.use(urlencoded({ extended: true }));
 
-export default app;
+app.get('/', (req, res, next) => {
+    res.status(200).json({ "hello": "hello" });
+});
+
+export default server;
--- bin/www.js
+++ bin/www.js
@@ -1,57 +1,27 @@
-#!/usr/bin/env node
 
-/**
- * Module dependencies.
- */
-
-var app = require('../app');
-var debug = require('debug')('api:server');
-var http = require('http');
-
-/**
- * Get port from environment and store in Express.
- */
+import server from '../app.js';
+import debug from 'debug';
 
 var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-
-var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
 
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
 
-/**
- * Normalize a port into a number, string, or false.
- */
-
 function normalizePort(val) {
   var port = parseInt(val, 10);
+  console.log(port);
 
   if (isNaN(port)) {
-    // named pipe
     return val;
   }
 
   if (port >= 0) {
-    // port number
     return port;
   }
 
   return false;
 }
-
-/**
- * Event listener for HTTP server "error" event.
- */
 
 function onError(error) {
   if (error.syscall !== 'listen') {
