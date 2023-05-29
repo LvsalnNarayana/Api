abd7d6f32b20032daeaa6118f706e46b9063fdcd
LvsalnNarayana <lvsalnnarayana@gmail.com>
2023-05-29 01:31:05 +0100

    final touch

--- app.js
+++ app.js
@@ -1,30 +1,17 @@
 import express, { urlencoded } from "express";
-import cors from "cors";
 import { createServer } from "http";
 import { sessionMiddleware } from "./utils/SessionMiddleware.js";
 import { store } from "./utils/SessionMiddleware.js";
-import { readFileSync } from "fs";
 
 var app = express();
 
-// const options = {
-//   key: readFileSync('./ssl/key.pem'),
-//   cert: readFileSync('./ssl/localhost.crt'),
-// };
 var server = createServer(app);
-
-app.use(
-  cors({
-    origin: "https://api-tester-48e59.web.app",
-    credentials: true,
-  })
-);
 
 app.use(sessionMiddleware);
 
 app.use(urlencoded({ extended: true }));
 
-app.get("/", (req, res, next) => {
+app.get("/", (req, res) => {
   req.session.user = "this is a test user";
   req.session.save();
   store.get(req.sessionID, (err, data) => {
@@ -32,23 +19,7 @@ app.get("/", (req, res, next) => {
       console.error(err);
       return res.sendStatus(500);
     }
-    res.setHeader('Access-Control-Allow-Origin', 'https://api-tester-48e59.web.app');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-requested-with');
-    res.cookie('myCookie', 'example-value', {
-      path: '/',
-      secure: true,
-      sameSite: 'none',
-      httpOnly: false,
-      domain: 'api-tester-3000.azurewebsites.net'
-    });
-    res.status(200).json({
-      "request headers": req.headers,
-      "cookies": req.cookies,
-      "sessionId": req.sessionID,
-      "session data": data,
-      "session request": req.session,
-    });
+    res.status(200).send({ message: "this is a success message" });
   });
 });
 
